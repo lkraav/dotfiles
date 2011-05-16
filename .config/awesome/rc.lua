be1e3e365f4e1bdbb4b0b0e03cf4472bf394b97f
leho <leho@kraav.com>
2011-05-16 12:45:40 +0300

    awesome: layout update

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -46,7 +46,6 @@ layouts =
 {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
@@ -54,6 +53,7 @@ layouts =
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
+    awful.layout.suit.tile.left,
     -- awful.layout.suit.max.fullscreen,
     -- awful.layout.suit.magnifier
 }
@@ -64,7 +64,7 @@ layouts =
 tags = {
     names  = { "1www", "2 @ ", "3trm", "4off", "5msg", "6vmr", 7, "8tom", "9log" },
     layout = { layouts[1], layouts[4], layouts[2], layouts[4], layouts[2],
-               layouts[2], layouts[1], layouts[1], layouts[2] }
+               layouts[2], layouts[1], layouts[2], layouts[2] }
 }
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
