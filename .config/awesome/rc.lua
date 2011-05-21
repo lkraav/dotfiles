3cd8b8e60d18877f317bc9b8a30831103288980a
leho <leho@kraav.com>
2011-05-21 16:49:28 +0300

    awesome: Mod+F10 screenshots ftw

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -313,6 +313,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey,		  }, "F10",   function () awful.util.spawn("import -window root " .. os.getenv("HOME") .. "/screenshot-" .. os.time() .. ".jpg") end),
 
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
