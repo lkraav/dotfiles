c562ed04dad0c1968baefd9dc8cba8665b5ab880
Leho Kraav <leho@kraav.com>
2012-05-22 17:32:31 +0300

    awesome: enable fair, magnified layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -75,12 +75,12 @@ layouts =
     awful.layout.suit.tile.top,
     -- awful.layout.suit.tile,
     -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.fair,
+    awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier
+    awful.layout.suit.magnifier,
 }
 -- }}}
 
